64fa30cee74eed07de488fb6896df25b81ad6965
WKWKA <ctd0606@naver.com>
2023-02-16 14:44:07 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,162 +1,35 @@
 <template>
-    <div class="container">
-        <h2>TODO-List</h2>
-        <input type="text" placeholder="검색" class="form-control" v-model="serchText">
-        <TodoSimpleForm  @add-todo="addTodo"/>
-        <div v-if="!filteredTodos.length">
-            찾는 문장이 없습니다..
-        </div>
-        <TodoList :todos="filteredTodos" @delete-todo="deleteTodo" @toggle-todo="toggleTodo" />
-        <br>
-        <nav class="nav">
-            <ul class="pagination">
-                <li v-if="currentPage !== 1" class="page-item"><a href="#" @click="getTodos(currentPage-1)" class="page-link">-</a></li>
-
-               <li v-for="page in numberOfPages" :key="page" class="page-item" :class="currentPage===page ? 'active' : ''">
-                    <a href="#" @click="getTodos(page)" class="page-link" :class="currentPage===page ? 'active' : ''">{{ page }}</a>
-                </li> 
-
-                <li v-if="numberOfPages !== currentPage" class="page-item"><a href="#" @click="getTodos(currentPage+1)" class="page-link">+</a></li>
-            </ul>
-        </nav>
-    </div>
+    <nav class="navbar">
+        <ul>
+            <router-link to="/">Home</router-link>
+            <router-link to="/todos">Todos</router-link>
+        </ul>
+        
+    </nav>
+    <router-view/>
 </template>
 
 <script>
-import { computed, ref } from 'vue';
-import TodoSimpleForm from './components/TodoSimpleForm.vue';
-import TodoList from './components/TodoList.vue';
-import axios from 'axios';
-
-export default {
-    components: {
-        TodoSimpleForm,
-        TodoList,
-    },
-    setup(){
+    export default {
         
-        const todos = ref([ ]);
-        const serchText= ref('');
-        const error=ref('');
-        const limit=5;
-        const numberOfTodos =ref(0);
-        const currentPage=ref(1);
-        const numberOfPages=computed(() =>{
-            return Math.ceil(numberOfTodos.value/limit)
-        });
-
-        const getTodos = async (page = currentPage.value) =>{
-            currentPage.value=page
-            try{
-                const res=await axios.get(`http://localhost:3000/todos?_page=${page}&_limit=${limit}`);
-                //console.log(res.headers)
-                numberOfTodos.value=res.headers['x-total-count'];
-                todos.value=res.data;
-            }catch (err){
-                console.log(err);
-                error.value="찾는 문장이 없습니다"
-            }
-        };
-        getTodos();
-        const addTodo = async (todo) => {
-            error.value='';
-            try{
-                const res=await axios.post('http://localhost:3000/todos', {
-                    subject:todo.subject,
-                    completed:todo.completed, 
-                })
-                todos.value.push(res.data);
-            }catch (err){
-                console.log(err);
-                error.value="잘못된 입력입니다."
-            }
-            
-        }
-
-        /* const addTodo = (todo) => {
-           // todos.value.push(todo);
-            error.value='';
-           axios.post('http://localhost:3000/todos',{
-                subject:todo.subject,
-                completed:todo.completed,
-           }).then((res) =>{
-                console.log(res);
-                todos.value.push(res.data)
-           }).catch((err) =>{
-                console.log(err);
-                error.value="잘못된 입력입니다."
-           });
-        } */
-       
-        const deleteTodo = async (index) => {
-            error.value="";
-            const id= todos.value[index].id;
-            try{
-                await axios.delete('http://localhost:3000/todos/'+ id);
-                todos.value.splice(index, 1);
-            }catch(err){
-                console.log(err);
-                error.value="찾는 문장이 없습니다"
-            }
-           
-        }
-        const toggleTodo= async (index) =>{
-           // console.log(index)
-           error.value="";
-           const id= todos.value[index].id;
-
-           try{
-                await axios.patch('http://localhost:3000/todos/'+ id, {
-                    completed: !todos.value[index].completed
-                });
-                todos.value[index].completed= !todos.value[index].completed;
-           }catch(err){
-                console.log(err);
-                error.value="찾는 문장이 없습니다"
-           }
-        }
-        const filteredTodos = computed(() =>{
-            if(serchText.value){
-                return todos.value.filter(todo =>{
-                    return todo.subject.includes(serchText.value);
-                })
-            }
-            return todos.value;
-        })
-
-        return {
-            todos,
-           /*  todoStyle, */
-           deleteTodo,
-           addTodo,
-           toggleTodo,
-           serchText,
-           filteredTodos,
-           getTodos,
-           numberOfPages,
-           currentPage,
-        }
     }
-}
 </script>
 
+
 <style>
-    *{margin: 0; padding: 0; box-sizing: border-box;}
-    .container{width: 100%; max-width: 1024px; margin: 0 auto; padding: 0 20px;}
-    h2{text-align: center; color: #147f8d; margin-bottom: 50px; margin-top: 50px;}
-    .d-flex{display: flex;}
-    .flex-grow-1{flex-grow: 1;}
-    .flex-grow-1 input{width: 98%; padding: 10px 20px;}
-    .btn{padding: 12px 30px; border: none; background: #147f8d; color: #fff}
-    form{margin-bottom: 50px;}
-    .card{margin: 10px 0;}
-    .card-body{border: 1px solid #ccc; padding: 10px 20px; display: flex; justify-content: space-between; align-items: center;}
-    .form-check-input{margin-right: 10px;}
-    .todoStyle{text-decoration: line-through;color: gray}
-    .btnR{padding: 5px 20px; background: #e93838; color: #fff; border: none}
-    .form-control{width: 100%; border: 1px solid #ddd; margin-bottom: 10px; padding: 10px 20px;}
-    .pagination{list-style-type: none; display: flex; justify-content: center;}
-    .page-item{ padding: 10px; margin: 0 3px; border: 1px solid #ddd}
-    .page-link{color: #666; text-decoration: none;}
-    .active{background: #666; color: #fff}
+    nav {
+    padding: 30px;
+    text-align: center;
+    margin: 0 auto;
+    }
+
+    nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    padding:  0 10px;
+    }
+
+    nav a.router-link-exact-active {
+    color: #42b983;
+    }
 </style>
